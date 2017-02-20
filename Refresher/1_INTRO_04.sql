14263c47f187c36f0c6f0fc762bf35d4036459bb
Tim Van Den Bosch <tim_vandenbosch@msn.com>
2017-02-20 09:49:27 +0100

    Polishing
    ~whitlines + capital

--- Refresher/1_INTRO_04.sql
+++ Refresher/1_INTRO_04.sql
@@ -2,7 +2,7 @@ select d.department_name department, count(e.employee_id) "aantal medewerkers"
 from departments d join employees e
 on d.department_id = e.department_id
 join locations l on d.location_id = l.location_id
-where l.city = 'seattle'
+where l.city = 'Seattle'
 group by d.department_name
 having count(e.employee_id) >= 5;
 /
